09ae0389134214db12653dedc650ba967beafadf
Akshay more <akshay@addwebsolution.in>
2023-10-04 18:56:26 +0530

    updateed

--- composer/phpmd.sh
+++ composer/phpmd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /var/lib/jenkins/workspace/code_analysis/phpmd_report
-vendor/bin/phpmd /var/lib/jenkins/workspace/code_analysis/wordpress/index.php xml codesize,controversial --reportfile /var/lib/jenkins/workspace/code_analysis/phpmd_report/report.xml > pmd.xml
+vendor/bin/phpmd /var/lib/jenkins/workspace/code_analysis/wordpress/index.php xml codesize,controversial --reportfile /var/lib/jenkins/workspace/code_analysis/phpmd_report/pmd.xml
