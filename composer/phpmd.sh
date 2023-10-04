582e037f04dbe8a950de805ef47d275c425e91fc
Akshay more <akshay@addwebsolution.in>
2023-10-04 18:33:14 +0530

    phpmd xml added2

--- composer/phpmd.sh
+++ composer/phpmd.sh
@@ -1,2 +1,2 @@
 cd /var/lib/jenkins/workspace/code_analysis/phpmd_report
-vendor/bin/phpmd /var/lib/jenkins/workspace/code_analysis/wordpress/index.php xml codesize,controversial --reportfile /var/lib/jenkins/workspace/code_analysis/phpmd_report/report.xml > report.xml
+vendor/bin/phpmd /var/lib/jenkins/workspace/code_analysis/wordpress/index.php xml codesize,controversial --reportfile /var/lib/jenkins/workspace/code_analysis/phpmd_report/report.xml > pmd.xml
