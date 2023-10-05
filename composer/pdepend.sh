96c46dd1c724ca72326a97bc6d9ffba4f4d87e3c
Akshay more <akshay@addwebsolution.in>
2023-10-05 17:40:43 +0530

    all

--- composer/pdepend.sh
+++ composer/pdepend.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-cd /var/lib/jenkins/workspace/pdepend/composer
+cd /var/lib/jenkins/workspace/checkstyle_php_loc_pdepend/composer
 
-pdepend --jdepend-xml=pdepend_summary.xml /var/lib/jenkins/workspace/pdepend/wordpress/
+pdepend --jdepend-xml=pdepend_summary.xml /var/lib/jenkins/workspace/checkstyle_php_loc_pdepend/wordpress/
 python3 convert_pdepend_to_checkstyle.py
 
