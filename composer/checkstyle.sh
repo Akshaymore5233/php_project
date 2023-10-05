ba6029466de07c83f9ae70194c301fc1a918ee62
Akshay more <akshay@addwebsolution.in>
2023-10-05 17:59:17 +0530

    all

--- composer/checkstyle.sh
+++ composer/checkstyle.sh
@@ -1,2 +1,3 @@
-cd /var/lib/jenkins/workspace/code_analysis/composer
-
+#! /bin/bash
+cd /var/lib/jenkins/workspace/checkstyle_report_phpcs/composer
+phpcs --report=checkstyle --report-file=checkstyle.xml /var/lib/jenkins/workspace/code_analysis/wordpress/index.php
