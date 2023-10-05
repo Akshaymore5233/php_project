168acf4e7d03721761c5a83791297ec511f3a3f1
Akshay more <akshay@addwebsolution.in>
2023-10-05 16:45:08 +0530

    updated findbug file

--- phpstan/phpstan.sh
+++ phpstan/phpstan.sh
@@ -3,3 +3,4 @@ pwd
 cd /var/lib/jenkins/workspace/phpstan_findbugs/phpstan
 pwd
 phpstan analyze --error-format=checkstyle > phpstan_checkstyle_report.xml
+php convert_phpstan_to_findbugs.php > findbugs_report.xml
